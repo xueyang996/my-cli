55813ca25c6a371e93dcfeae820565ad18069dad
大黄蜂(张明瑞) <zhangmingrui@mail.i-i.ai>
2020-11-29 14:21:03 +0800

    ✨feat(dclone添加):
    git clone 替换为 dclone包

--- README.md
+++ README.md
@@ -1,4 +1,38 @@
-git clone https://github.com/xueyang996/my-cli.git
+全局安装使用
+```
+cnpm i -g ii-cli dclone
+```
+
+### 常用命令
+查看命令
+```
+ii-cli help
+```
+增加模板
+```
+ii-cli add 
+```
+复制模板
+```
+ii-cli init
+```
+删除模板
+```
+ii-cli delete
+```
+模板列表
+```
+ii-cli list
+```
+
+
+git clone https://github.com/xueyang996/ii-cli.git
 
 npm link
-在需要的地方输入my-cli即可使用
+在需要的地方输入ii-cli即可使用
+
+### 注意
+由于该cli命令使用dclone命令，需要全局安装
+cnpm i -g dclone
+
+当前只支持安装到当前目录
--- bin/cmd/init.js
+++ bin/cmd/init.js
@@ -3,8 +3,26 @@ const inquirer = require('inquirer');
 
 const { templateMap, log } = require('../utils');
 
+function cloneGit(giturl) {
+  const cloneRepo = spawn('dclone', [giturl]);
+  cloneRepo.stdout.on('data', data => {
+    log.info(data)
+  })
+  cloneRepo.stderr.on('data', data => {
+    log.error(data)
+  })
+  cloneRepo.stderr.on('close', code => {
+    log.success('下载结束，退出码为：'+ code)
+    process.exit()
+  })
+}
+
 module.exports = () => {
   const choices = Object.keys(templateMap || {})
+  const url = process.argv.splice(3)[0]
+  if(url) {
+    return cloneGit(url)
+  }
   return inquirer
   .prompt([
     {name: "name", message: '初始化模板：',  type: 'list', choices},
@@ -17,17 +35,8 @@ module.exports = () => {
       process.exit()
     }
     const { giturl, branch, } = templateMap[name]
-    const cloneRepo = spawn('git', ['clone', '--branch', branch, giturl, fileName]);
-    cloneRepo.stdout.on('data', data => {
-      log.info(data)
-    })
-    cloneRepo.stderr.on('data', data => {
-      log.error(data)
-    })
-    cloneRepo.stderr.on('close', code => {
-      log.success('下载结束，退出码为：'+ code)
-      process.exit()
-    })
+    // const cloneRepo = spawn('git', ['clone', '--branch', branch, giturl, fileName]);
+    cloneGit(giturl)
   })
   .catch(error => {
     if(error.isTtyError) {
